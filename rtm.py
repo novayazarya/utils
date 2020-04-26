bb8ac7c852a017d20ce1c6fb8fcb00dd77d4168c
Popov Mikhail <popov@popov.mn>
2020-04-26 21:59:57 +0300

    РТМ 0001.01-99

--- rtm.py
+++ rtm.py
@@ -1,6 +1,10 @@
 import math
 
 def get_full_rtm(arg):
+    """
+    python rtm.py 142117 16 00738
+
+    """
     code = ([int(y) for x in arg[0] for y in x] if isinstance(arg, list)
                                                 else [int(x) for x in arg if x.isdigit()])
     assert len(code) == 13
@@ -12,8 +16,8 @@ def get_full_rtm(arg):
     else:
         r = 0
     return ''.join(map(str, code)) + str(r)
-    #print(args,code)
+
 if __name__ == '__main__':
     import sys
-    print(get_full_rtm('142117 16 00738'))
-    #print(get_full_rtm(sys.argv[1:]))
+
+    print(get_full_rtm(sys.argv[1:]))
